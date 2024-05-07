dc171bddb292b65d7e0eb3388859c61119fc4170
noahJ101 <37839884+noahJ101@users.noreply.github.com>
2024-05-07 10:13:41 +0100

    Auth

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-   public function dasboard()
+   public function dashboard()
    {
     return view('backend.dashboard');
    }
